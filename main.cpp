eac0176e2c86e181f9257d0872284307e8dff711
Nathan Emigh <nathanemigh@ACL-P1C13.cis>
2017-11-14 13:54:34 -0800

    update

--- main.cpp
+++ main.cpp
@@ -20,53 +20,86 @@ class Bank
       double add_balance(double a);
       double average_daily(double a);
       double min_daily(double a);
-      
+      double get_average();
+      void set_previous_day(int a);
    private:
       double total_balance;
       double interest_rate;
+      double total_interest;
+      int previous_day;
 };
 
 Bank::Bank()
 {
    total_balance = 0.0;
-   interest_rate= 1.5;
+   interest_rate= 1.005;
 }
 
 void Bank::print()
 {
+   std::cout << "Total Interest is: " << total_interest << std::endl;
    std::cout << "Current balance is: " << total_balance << std::endl;
+}
+
+void Bank::set_previous_day(int a)
+{
+   previous_day = a;
 }
 
 void Bank::get_interest(int day)
 {
-   if(day >=30)
+  /* if(day >=30)
    {
       for(int i = 30; i <= day; i++)
       {
          if(!(i % 30))
          {
-            total_balance = total_balance * interest_rate;
+            total_balance *=  interest_rate;
             std::cout << "Current Balance with interest is: " << total_balance << std::endl;
          }
       }
-   }
+   }*/
+     
+    std::cout << day << " " <<  previous_day << std::endl;  
+    
+    int i = (day / 30) * 30;
+
+    if( i > previous_day && i < day)
+      std::cout << "Lloyd is a bundle of sticks" << std::endl;
+         
 }
 
 double Bank::add_balance(double a)
 {
-   total_balance = total_balance + a;
+   total_balance = total_balance + a; 
 }
 
 double Bank::average_daily(double a)
 {
-   int day;
-   if(day > 30)
+/*
+   double average;  
+   double average_outcome;    
+
+   std::vector<double> maths;
+   
+   maths.push_back(a);
+
+   std::cout << "things" << std::endl;
+
+   for(int i = 0; i < maths; i++)
    {
-      for(int i = 30; i > day; i+30)
-      {
-      
-      }
+     average = average + a;
    }
+ 
+   average_outcome = average / i;
+*/
+}
+
+
+
+double Bank::get_average()
+{
+   return 0; 
 }
 
 double Bank::min_daily(double a = 35)
@@ -86,7 +119,7 @@ class Transaction
       void print() const; 
       int get_day();
       double get_amount();
-      
+                
    private:
       int day;
       double amount;
@@ -148,17 +181,22 @@ int main()
       
       temp_addtobal = t.get_amount();
       b.add_balance(temp_addtobal);
-      day = t.get_day();
-      b.print();
-      b.get_interest(day);
-      b.min_daily();
       
+      day = t.get_day();
+      
+      b.get_interest(day);
+      
+     // b.min_daily();
+     // b.average_daily(temp_addtobal);
+      b.set_previous_day(t.get_day());
       transaction.push_back(t);
+      b.print();
       std::cout << "Enter another Transaction? y or n" << std::endl;
       char response;
       std::cin >> response;      
       if(response != 'y' && response != 'Y')
          more = false;
+      
     }
 
     print_transactions(transaction);
